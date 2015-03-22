5edd318eebee90808bd9ef9e391caa3cadcba0f9
Rickard Lindberg <ricli85@gmail.com>
2015-03-22 16:54:09 +0100

    Clean up Hakyll code.

--- site.hs
+++ site.hs
@@ -7,122 +7,127 @@ import Hakyll
 import System.Environment
 import System.FilePath
 
-main :: IO ()
-main = hasHakyllBuildTarget "webserver" >>= \shouldDeIndexUrls -> hakyll $ do
+rules :: (Item String -> Compiler (Item String)) -> Rules ()
+rules processUrls = do
 
-    let processUrls x = if shouldDeIndexUrls
-                           then relativizeUrls x >>= deIndexUrls
-                           else relativizeUrls x
+    match "templates/*" $ do
+        compile templateCompiler
 
-    match "index.markdown" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/title.html" defaultContext
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= processUrls
+    match "css/**" $ do
+        verbatimCopy
+    match "images/**" $ do
+        verbatimCopy
+    match "writing/**/*.png" $ do
+        verbatimCopy
+    match "writing/**/*.jpg" $ do
+        verbatimCopy
 
-    match "projects/index.textile" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/title.html" defaultContext
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= processUrls
-
-    let reflectionsOnProgrammingContext = listField "posts" postContext (loadAll "writing/reflections-on-programming/*.textile")
-                                          `mappend`
-                                          defaultContext
-
-    match "writing/reflections-on-programming/index.markdown" $ do
-        route $ setExtension "html"
-        compile $ getResourceBody
-            >>= applyAsTemplate reflectionsOnProgrammingContext
-            >>= return . renderPandoc
-            >>= loadAndApplyTemplate "templates/title.html" defaultContext
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= processUrls
-
-    match "writing/reflections-on-programming/*.textile" $ do
-        route $ customRoute (\identifier ->
-            let filePath = toFilePath identifier
-            in  takeDirectory filePath
-                </> takeBaseName filePath
-                </> "index.html")
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/title.html" postContext
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= processUrls
+    match ("index.markdown" .||.
+           "projects/index.textile") $ do
+        page processUrls
 
     match "writing/ardour-latency-free-overdubbing/index.rst" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= processUrls
+        postWithOwnTitle processUrls
 
     match "writing/xmodmap-on-fedora/index.markdown" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/title.html" postContext
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= processUrls
+        post processUrls
 
-    post processUrls "writing/python-danger-implicit-if/index.markdown"
+    match "writing/python-danger-implicit-if/index.markdown" $ do
+        post processUrls
 
-    let thoughtOfTheDayContext = listField "thoughts" postContext (loadAll "writing/thought-of-the-day/thoughts/*")
-                                 `mappend`
-                                 listField "thoughts2" postContext (loadAll "writing/thought-of-the-day/thoughts2/*")
-                                 `mappend`
-                                 defaultContext
+    match "writing/reflections-on-programming/index.markdown" $ do
+        pageAsTemplate
+            (createPostsContext "writing/reflections-on-programming/*.textile")
+            processUrls
+    match "writing/reflections-on-programming/*.textile" $ do
+        routeIndex
+        compilePost processUrls
 
     match "writing/thought-of-the-day/index.markdown" $ do
-        route $ setExtension "html"
-        compile $ getResourceBody
-            >>= applyAsTemplate thoughtOfTheDayContext
-            >>= return . renderPandoc
-            >>= loadAndApplyTemplate "templates/title.html" defaultContext
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= processUrls
-
+        pageAsTemplate
+            (listField "thoughts" postContext (loadAll "writing/thought-of-the-day/thoughts/*")
+             `mappend`
+             listField "thoughts2" postContext (loadAll "writing/thought-of-the-day/thoughts2/*")
+             `mappend`
+             defaultContext)
+            processUrls
     match "writing/thought-of-the-day/thoughts/*.markdown" $ do
-        route $ customRoute (\identifier ->
-            let filePath = toFilePath identifier
-            in  (takeDirectory . takeDirectory) filePath
-                </> takeBaseName filePath
-                </> "index.html")
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/title.html" postContext
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= processUrls
-
+        routeUpIndex
+        compilePost processUrls
     match "writing/thought-of-the-day/thoughts2/*.markdown" $ do
-        route $ customRoute (\identifier ->
-            let filePath = toFilePath identifier
-            in  (takeDirectory . takeDirectory) filePath
-                </> takeBaseName filePath
-                </> "index.html")
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/title.html" postContext
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= processUrls
+        routeUpIndex
+        compilePost processUrls
 
-    verbatimCopy "css/**"
-    verbatimCopy "images/**"
-    verbatimCopy "writing/**/*.png"
-    verbatimCopy "writing/**/*.jpg"
-
-    match "templates/*" $ compile templateCompiler
-
-verbatimCopy :: Pattern -> Rules ()
-verbatimCopy pattern = match pattern $ do
+verbatimCopy :: Rules ()
+verbatimCopy = do
     route idRoute
     compile copyFileCompiler
 
-post :: (Item String -> Compiler (Item String)) -> Pattern -> Rules ()
-post processUrls pattern = match pattern $ do
+page :: (Item String -> Compiler (Item String)) -> Rules ()
+page processUrls = do
     route $ setExtension "html"
     compile $ pandocCompiler
-        >>= loadAndApplyTemplate "templates/title.html" postContext
+        >>= loadAndApplyTemplate "templates/title.html" defaultContext
         >>= loadAndApplyTemplate "templates/default.html" defaultContext
         >>= processUrls
+
+pageAsTemplate :: Context String -> (Item String -> Compiler (Item String)) -> Rules ()
+pageAsTemplate context processUrls = do
+    route $ setExtension "html"
+    compile $ getResourceBody
+        >>= applyAsTemplate context
+        >>= return . renderPandoc
+        >>= loadAndApplyTemplate "templates/title.html" defaultContext
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= processUrls
+
+post :: (Item String -> Compiler (Item String)) -> Rules ()
+post processUrls = do
+    route $ setExtension "html"
+    compilePost processUrls
+
+postWithOwnTitle :: (Item String -> Compiler (Item String)) -> Rules ()
+postWithOwnTitle processUrls = do
+    route $ setExtension "html"
+    compile $ pandocCompiler
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= processUrls
+
+createPostsContext :: Pattern -> Context String
+createPostsContext pattern =
+    listField "posts" postContext (loadAll pattern)
+    `mappend`
+    defaultContext
+
+compilePost :: (Item String -> Compiler (Item String)) -> Rules ()
+compilePost processUrls = compile $ pandocCompiler
+    >>= loadAndApplyTemplate "templates/title.html" postContext
+    >>= loadAndApplyTemplate "templates/default.html" defaultContext
+    >>= processUrls
+
+routeUpIndex :: Rules ()
+routeUpIndex = route $ customRoute (\identifier ->
+    let filePath = toFilePath identifier
+    in  (takeDirectory . takeDirectory) filePath
+        </> takeBaseName filePath
+        </> "index.html")
+
+routeIndex :: Rules ()
+routeIndex = route $ customRoute (\identifier ->
+    let filePath = toFilePath identifier
+    in  takeDirectory filePath
+        </> takeBaseName filePath
+        </> "index.html")
+
+postContext :: Context String
+postContext = dateField "date" "%e %B %Y" `mappend`
+              defaultContext
+
+main :: IO ()
+main = hasHakyllBuildTarget "webserver" >>= \shouldDeIndexUrls -> hakyll $ do
+    rules $ \x -> if shouldDeIndexUrls
+       then relativizeUrls x >>= deIndexUrls
+       else relativizeUrls x
 
 hasHakyllBuildTarget :: String -> IO Bool
 hasHakyllBuildTarget target = fmap (elem ("HAKYLL_BUILD_TARGET", target))
@@ -136,7 +141,3 @@ stripIndexHtml url =
     if "index.html" `isSuffixOf` url && (head url) `elem` "/."
         then take (length url - 10) url
         else url
-
-postContext :: Context String
-postContext = dateField "date" "%e %B %Y" `mappend`
-              defaultContext
