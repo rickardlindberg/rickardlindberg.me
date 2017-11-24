0ede40899b66359e6d829d80bfe5bb9d0bce2bdd
Rickard Lindberg <ricli85@gmail.com>
2017-11-24 07:47:59 +0100

    Add TODOs.

--- writing/draft-rlmeta/rlmeta/bootstrap.py
+++ writing/draft-rlmeta/rlmeta/bootstrap.py
@@ -37,6 +37,12 @@ def main():
     b.make(src("8"), pycompiler("7"), pyout("8"), [
         "use repetition",
     ])
+    # TODO: Prelude with empty and space
+    # TODO: Remove space from regex
+    # TODO: Support parenthesis (anonymous rules)
+    # TODO: Distinguish re patterns / text patterns / output patterns
+    # TODO: Prevent repetition of output since it will create infinite loop?
+    # TODO: Memoize results? Speed up PEG & more elegant solution?
 
 
 class Bootstrapper(object):
