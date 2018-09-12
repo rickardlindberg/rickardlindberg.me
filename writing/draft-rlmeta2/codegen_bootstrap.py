0c931abfba195e2375c957ff324facb4b90a27bc
Rickard Lindberg <ricli85@gmail.com>
2018-09-12 09:25:24 +0200

    Work more on grammar.

--- writing/draft-rlmeta2/codegen_bootstrap.py
+++ writing/draft-rlmeta2/codegen_bootstrap.py
@@ -1,7 +1,3 @@
 class CodeGenerator(_RLMeta):
     def ast(self):
-        _ors = []
-        def _label1():
-            ...
-        _ors.append(_label1)
-        return self._or(_ors)
+        ...
