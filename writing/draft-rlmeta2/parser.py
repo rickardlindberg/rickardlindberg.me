97fd90a4ed064b6b6ce542bbac8977c996cfe235
Rickard Lindberg <ricli85@gmail.com>
2018-09-25 19:59:31 +0200

    Flesh out Parser._rule_rule.

--- writing/draft-rlmeta2/parser.py
+++ writing/draft-rlmeta2/parser.py
@@ -41,14 +41,48 @@ class Parser(_RLMeta):
             ])
         )()
 
+    def _rule_rule(self):
+        return (lambda:
+            self._or([
+                (lambda:
+                    (lambda _vars:
+                        (lambda:
+                            self._and([
+                                (lambda:
+                                    _vars.bind("x", (lambda:
+                                        self._match("name")
+                                    )())
+                                ),
+                                (lambda:
+                                    self._match("space")
+                                ),
+                                (lambda:
+                                    self._match_charsec("=")
+                                ),
+                                (lambda:
+                                    _vars.bind("y", (lambda:
+                                        self._match("choices")
+                                    )())
+                                ),
+                                (lambda:
+                                    _SemanticAction(lambda: (["Rule"]+[_vars.lookup("x").eval()]+[_vars.lookup("y").eval()]+[]))
+                                ),
+                            ])
+                        )()
+                    )(_Vars())
+                ),
+            ])
+        )()
+
     def _rule_name(self):
         return self._match_charsec("n")
+
+    def _rule_choices(self):
+        return self._match_charsec("c")
 
     def _rule_space(self):
         return self._match_charsec(" ")
 
-    def _rule_rule(self):
-        return self._match_charsec("r")
 
-
-print(Parser().run("grammar", "n {rrrrr }"))
+import pprint
+pprint.pprint(Parser().run("grammar", "n {n =c }"))
