ce85dbac9d99421e4f394c6a994a4567b96cf3ca
Rickard Lindberg <ricli85@gmail.com>
2018-09-26 08:31:21 +0200

    Cleaner calculator runtime.

--- writing/draft-rlmeta2/calculator_runtime.py
+++ writing/draft-rlmeta2/calculator_runtime.py
@@ -1,5 +1,7 @@
 from operator import add, mul
-calculator = Calculator()
-while True:
-    line = raw_input("> ")
-    print(str(calculator.run("expression", line)))
+
+if __name__ == "__main__":
+    calculator = Calculator()
+    while True:
+        line = raw_input("> ")
+        print(calculator.run("expression", line))
