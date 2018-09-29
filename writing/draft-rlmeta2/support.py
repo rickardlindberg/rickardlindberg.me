8e8c6366a8b6695084f7fb7467c32decef4f6656
Rickard Lindberg <ricli85@gmail.com>
2018-09-29 14:34:26 +0200

    Clarify names.

--- writing/draft-rlmeta2/support.py
+++ writing/draft-rlmeta2/support.py
@@ -4,7 +4,7 @@ class _RLMeta(object):
         self._log = (lambda message: None) if logger is None else logger
 
     def run(self, rule_name, input_object):
-        self._input = _Input.from_object(input_object)
+        self._stream = _Stream.from_object(input_object)
         self._memo = {}
         try:
             result = self._match(rule_name).eval()
@@ -25,22 +25,23 @@ class _RLMeta(object):
             self._log("matched {: <20} {} -> {}\n".format(*item))
 
     def _match(self, rule_name):
-        key = (rule_name, self._input.as_key())
+        key = (rule_name, self._stream.memo_key())
         if key in self._memo:
-            result, _, self._input = self._memo[key]
+            result, _, self._stream = self._memo[key]
         else:
-            start_input = self._input
+            start = self._stream
             result = getattr(self, "_rule_{}".format(rule_name))()
-            self._memo[key] = (result, start_input, self._input)
+            end = self._stream
+            self._memo[key] = (result, start, end)
         return result
 
     def _or(self, matchers):
-        saved_input = self._input
+        original_stream = self._stream
         for matcher in matchers:
             try:
                 return matcher()
             except _MatchError:
-                self._input = saved_input
+                self._stream = original_stream
         raise _MatchError()
 
     def _and(self, matchers):
@@ -52,15 +53,15 @@ class _RLMeta(object):
     def _star(self, matcher):
         result = []
         while True:
-            saved_input = self._input
+            original_stream = self._stream
             try:
                 result.append(matcher())
             except _MatchError:
-                self._input = saved_input
+                self._stream = original_stream
                 return _SemanticAction(lambda: [x.eval() for x in result])
 
     def _negative_lookahead(self, matcher):
-        saved_input = self._input
+        original_stream = self._stream
         try:
             matcher()
         except _MatchError:
@@ -68,17 +69,17 @@ class _RLMeta(object):
         else:
             raise _MatchError()
         finally:
-            self._input = saved_input
+            self._stream = original_stream
 
     def _match_range(self, start, end):
-        next_objext, self._input = self._input.next()
+        next_objext, self._stream = self._stream.next()
         if next_objext >= start and next_objext <= end:
             return _SemanticAction(lambda: next_objext)
         else:
             raise _MatchError()
 
     def _match_string(self, string):
-        next_object, self._input = self._input.next()
+        next_object, self._stream = self._stream.next()
         if next_object == string:
             return _SemanticAction(lambda: string)
         else:
@@ -86,33 +87,33 @@ class _RLMeta(object):
 
     def _match_charseq(self, charseq):
         for char in charseq:
-            next_object, self._input = self._input.next()
+            next_object, self._stream = self._stream.next()
             if next_object != char:
                 raise _MatchError()
         return _SemanticAction(lambda: charseq)
 
     def _any(self):
-        next_object, self._input = self._input.next()
+        next_object, self._stream = self._stream.next()
         return _SemanticAction(lambda: next_object)
 
     def _match_list(self, matcher):
-        next_object, next_input = self._input.next()
+        next_object, next_input = self._stream.next()
         if isinstance(next_object, list):
-            self._input = self._input.nested(next_object)
+            self._stream = self._stream.nested(next_object)
             matcher()
-            if self._input.is_at_end():
-                self._input = next_input
+            if self._stream.is_at_end():
+                self._stream = next_input
                 return _SemanticAction(lambda: next_object)
         raise _MatchError()
 
-class _Input(object):
+class _Stream(object):
 
     @classmethod
     def from_object(cls, input_object):
         if isinstance(input_object, basestring):
-            return _StringInput(list(input_object))
+            return _CharStream(list(input_object))
         else:
-            return _ListInput([input_object])
+            return _ObjectStream([input_object])
 
     def __init__(self, objects):
         self._objects = objects
@@ -129,43 +130,43 @@ class _Input(object):
     def is_at_end(self):
         return len(self._objects) == 0
 
-class _StringInput(_Input):
+class _CharStream(_Stream):
 
     def __init__(self, objects, line=1, column=1):
-        _Input.__init__(self, objects)
+        _Stream.__init__(self, objects)
         self._line = line
         self._column = column
 
-    def as_key(self):
+    def memo_key(self):
         return (self._line, self._column)
 
     def _advance(self, next_object, objects):
         if next_object == "\n":
-            return _StringInput(objects, self._line+1, 1)
+            return _CharStream(objects, self._line+1, 1)
         else:
-            return _StringInput(objects, self._line, self._column+1)
+            return _CharStream(objects, self._line, self._column+1)
 
     def __str__(self):
         return "L{:03d}:C{:03d}".format(self._line, self._column)
 
-class _ListInput(_Input):
+class _ObjectStream(_Stream):
 
     def __init__(self, objects, parent=(), pos=0):
-        _Input.__init__(self, objects)
+        _Stream.__init__(self, objects)
         self._parent = parent
         self._pos = pos
 
-    def as_key(self):
+    def memo_key(self):
         return self._parent + (self._pos,)
 
     def nested(self, input_object):
-        return _ListInput(input_object, self._parent+(self._pos,))
+        return _ObjectStream(input_object, self._parent+(self._pos,))
 
     def _advance(self, next_object, objects):
-        return _ListInput(objects, self._parent, self._pos+1)
+        return _ObjectStream(objects, self._parent, self._pos+1)
 
     def __str__(self):
-        return "[{}]".format(", ".join(str(x) for x in self.as_key()))
+        return "[{}]".format(", ".join(str(x) for x in self.memo_key()))
 
 class _SemanticAction(object):
 
