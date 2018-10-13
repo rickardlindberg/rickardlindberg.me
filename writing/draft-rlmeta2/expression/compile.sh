4770a5793cd9e422077809a1928f34113ca0769f
Rickard Lindberg <ricli85@gmail.com>
2018-10-13 20:06:27 +0200

    Work on prose.

--- writing/draft-rlmeta2/expression/compile.sh
+++ writing/draft-rlmeta2/expression/compile.sh
@@ -18,11 +18,11 @@ $parser_py
 $codegenerator_py
 
 if __name__ == "__main__":
-    parser = ExpressionParser(logger=sys.stderr.write)
-    codegenerator = ExpressionCodegenerator(logger=sys.stderr.write)
+    parser = Parser(logger=sys.stderr.write)
+    codegenerator = CodeGenerator(logger=sys.stderr.write)
     while True:
         line = raw_input("> ")
         ast = parser.run("expression", line)
-        output = codegenerator.run("ast", ast)
-        sys.stdout.write(output)
+        assembly = codegenerator.run("ast", ast)
+        sys.stdout.write(assembly)
 EOF
