9439401616013fab2e935c01979afc47d21f4fec
Rickard Lindberg <ricli85@gmail.com>
2019-04-22 08:28:33 +0200

    Reset rlmeta.

--- writing/draft-rlmeta-optimizing/compile.sh
+++ writing/draft-rlmeta-optimizing/compile.sh
@@ -12,16 +12,11 @@ to_python_string() {
 
 support_py=$(cat support.py)
 support_py_string=$(to_python_string < support.py)
-echo "PARSER" 1>&2
 parser_py=$(python "$rlmeta_compiler" < parser.rlmeta)
-echo "CODEGENERATOR" 1>&2
 codegenerator_py=$(python "$rlmeta_compiler" < codegenerator.rlmeta)
 
 cat <<EOF
 import sys
-import time
-import cProfile
-import pstats
 
 SUPPORT = $support_py_string
 
@@ -33,33 +28,17 @@ $codegenerator_py
 
 join = "".join
 
-def profile(name, fn, *args):
-    t1 = time.time()
-    result = fn(*args)
-    t2 = time.time()
-    sys.stderr.write("{} {}s\\n".format(name, t2-t1))
-    return result
-
 def compile_grammar(grammar):
     parser = Parser()
     code_generator = CodeGenerator()
-    ast = profile("parser:       ", parser.run, "grammar", grammar)
-    code = profile("codegenerator:", code_generator.run, "ast", ast)
-    return code
+    return code_generator.run("ast", parser.run("grammar", grammar))
 
 if __name__ == "__main__":
     if "--support" in sys.argv:
         sys.stdout.write(SUPPORT)
     else:
         try:
-            p = cProfile.Profile()
-            p.enable()
             sys.stdout.write(compile_grammar(sys.stdin.read()))
-            p.disable()
-            s = StringIO()
-            ps = pstats.Stats(p, stream=s).sort_stats("tottime")
-            ps.print_stats(0.01)
-            sys.stderr.write("\n" + s.getvalue() + "\n")
         except _MatchError as e:
             sys.stderr.write(e.describe())
             sys.exit(1)
