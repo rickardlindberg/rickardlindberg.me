ffe7a73e3acc24c99fb7db59774c6b6f11319061
Rickard Lindberg <ricli85@gmail.com>
2019-06-26 11:21:07 +0200

    Write.

--- writing/draft-rlmeta-optimizing/graphs.py
+++ writing/draft-rlmeta-optimizing/graphs.py
@@ -14,14 +14,14 @@ def add(name, overall, single):
 
 add('base', 0.756, 0.476)
 add('avoid slicing', 0.608, 0.415)
-add('faster string concatenation', 0.603, 0.397)
+add('faster string\nconcatenation', 0.603, 0.397)
 add('and/or', 0.592, 0.380)
-add('remove newlines', 0.565, 0.343)
-add('match call rule', 0.406, 0.220)
-add('optimize position', 0.392, 0.220)
-add('lazy fail', 0.370, 0.204)
+add('remove\nnewlines', 0.565, 0.343)
+add('match call\nrule', 0.406, 0.220)
+add('optimize\nposition', 0.392, 0.220)
+add('lazy fail\nmessages', 0.370, 0.204)
 add('peek', 0.351, 0.187)
-add('last or', 0.344, 0.185)
+add('last or\nexception', 0.344, 0.185)
 
 ind = np.arange(len(overalls))  # the x locations for the groups
 width = 0.35  # the width of the bars
@@ -34,6 +34,8 @@ ax.set_ylabel('Compilation time (s)')
 ax.set_xticks(ind)
 ax.set_xticklabels(labels)
 ax.legend()
+
+plt.xticks(rotation=70)
 
 fig.tight_layout()
 
