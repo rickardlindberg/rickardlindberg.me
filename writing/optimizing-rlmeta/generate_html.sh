0c75c4e09d5fb1f45927fe8fbc4535bd8b351ee1
Rickard Lindberg <ricli85@gmail.com>
2019-06-28 09:31:35 +0200

    Finialize optimizing rlmeta.

---
+++ writing/optimizing-rlmeta/generate_html.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+gen() {
+    html=$(rliterate rlmeta.rliterate --html)
+    echo "---"
+    echo "title: '$(echo "$html" | head -n1 | cut -c52- | cut -d'<' -f1)'"
+    echo "date: 2019-06-28"
+    echo "tags: rlmeta"
+    echo "---"
+    echo "$html" | tail -n+2
+}
+
+gen > index.html
