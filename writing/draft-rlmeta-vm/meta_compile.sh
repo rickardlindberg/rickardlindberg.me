4d262d6902cf8cccd8d04cfda6efcf2a09730787
Rickard Lindberg <ricli85@gmail.com>
2019-07-06 21:44:18 +0200

    Restore meta compile script.

--- writing/draft-rlmeta-vm/meta_compile.sh
+++ writing/draft-rlmeta-vm/meta_compile.sh
@@ -10,13 +10,9 @@ cd "$(dirname "$0")"
 
 ./compile.sh rlmeta2.py > rlmeta3.py
 
-./compile.sh rlmeta3.py > rlmeta4.py
-
-diff rlmeta3.py rlmeta4.py
+diff rlmeta2.py rlmeta3.py
 
 diff support.py <(python rlmeta3.py --support)
-
-mv rlmeta4.py rlmeta3.py
 
 mv rlmeta3.py rlmeta2.py
 
