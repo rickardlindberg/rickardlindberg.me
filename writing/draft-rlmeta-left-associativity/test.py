abb214ad30f0f09699cc1c4795843a697cd37e79
Rickard Lindberg <ricli85@gmail.com>
2019-08-21 21:32:26 +0200

    Write.

---
+++ writing/draft-rlmeta-left-associativity/test.py
@@ -0,0 +1,36 @@
+def parse():
+    return [
+        1,
+        ['-', 2],
+        ['-', 3],
+    ]
+
+def left_assoc(items):
+    expr = items.pop(0)
+    while items:
+        op, rhs = items.pop(0)
+        expr = [op, expr, rhs]
+    return expr
+
+def right_assoc(items):
+    expr = items.pop(0)
+    if items:
+        op, rhs = items.pop(0)
+        return [op, expr, right_assoc([rhs]+items)]
+    return expr
+
+def eval_expr(expr):
+    if isinstance(expr, list):
+        op, lhs, rhs = expr
+        if op == "-":
+            return eval_expr(lhs) - eval_expr(rhs)
+        else:
+            raise Exception("unknown operator {!r}".format(op))
+    else:
+        return expr
+
+def echo_eval_expr(expr):
+    print("{} => {}".format(expr, eval_expr(expr)))
+
+echo_eval_expr(left_assoc(parse()))
+echo_eval_expr(right_assoc(parse()))
