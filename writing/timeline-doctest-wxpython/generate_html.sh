3838ab7f2190a321eff8afa24c1b68fca1fe3d9d
Rickard Lindberg <ricli85@gmail.com>
2019-08-31 10:00:07 +0200

    Write.

---
+++ writing/timeline-doctest-wxpython/generate_html.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+gen() {
+    html=$(rliterate index.rliterate --html)
+    echo "---"
+    echo "title: '$(echo "$html" | head -n1 | cut -c52- | cut -d'<' -f1)'"
+    echo "date: 2019-08-31"
+    echo "tags: timeline"
+    echo "---"
+    echo "$html" | tail -n+2
+}
+
+gen > index.html
