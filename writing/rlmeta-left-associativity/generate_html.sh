3beeedfa1b1facbb7c7473b96b90513b0a9e8938
Rickard Lindberg <ricli85@gmail.com>
2019-09-07 14:11:47 +0200

    Write.

--- writing/rlmeta-left-associativity/generate_html.sh
+++ writing/rlmeta-left-associativity/generate_html.sh
@@ -5,7 +5,7 @@ gen() {
     echo "---"
     echo "title: '$(echo "$html" | head -n1 | cut -c52- | cut -d'<' -f1)'"
     echo "date: 2019-09-07"
-    echo "tags: rlmeta,draft"
+    echo "tags: rlmeta"
     echo "---"
     echo "$html" | tail -n+2
 }
