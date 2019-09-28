d1b3055b297f400018fe90c645d84faf77b980ec
Rickard Lindberg <ricli85@gmail.com>
2019-09-28 07:32:24 +0200

    Write.

--- writing/rlmeta-memoize-failures/graphs.py
+++ writing/rlmeta-memoize-failures/graphs.py
@@ -15,12 +15,12 @@ def add(name, overall, single):
 # 1. time ./compile.sh rlmeta.py > /dev/null
 # 2. python -m cProfile -s tottime rlmeta.py < parser.rlmeta
 
-add('optimized\nno memo', 1.460, 0.834)
-add('optimized', 0.344, 0.185)
-add('optimized\nfixed memo', 0.333, 0.145)
-add('vm\nno memo', 0.998, 0.502)
-add('vm', 0.254, 0.104)
-add('vm\nfixed memo', 0.241, 0.087)
+add('optimized\nno memo', 1.460, 0.841)
+add('optimized\noriginal', 0.344, 0.185)
+add('optimized\nmemo failures', 0.333, 0.145)
+add('vm\nno memo', 0.998, 0.518)
+add('vm\noriginal', 0.254, 0.104)
+add('vm\nmemo failures', 0.241, 0.087)
 
 ind = np.arange(len(overalls))  # the x locations for the groups
 width = 0.35  # the width of the bars
