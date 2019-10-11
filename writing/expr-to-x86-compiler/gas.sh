6c808bbbac2e813bc89184f93bc12a237f50073e
Rickard Lindberg <ricli85@gmail.com>
2019-10-11 08:17:53 +0200

    Write.

--- writing/expr-to-x86-compiler/gas.sh
+++ writing/expr-to-x86-compiler/gas.sh
@@ -5,7 +5,7 @@ cat | bash compile.sh parser acodegen xcodegen gas @expr.s
 gcc driver.c expr.s -o expr
 
 echo ""
-echo "=V======= Compile & run ========"
+echo "=V===== GccCompileAndRun ======="
 echo ""
 
 ./expr
--- writing/expr-to-x86-compiler/support.py
+++ writing/expr-to-x86-compiler/support.py
@@ -1,3 +1,69 @@
+def pad(text, suffix):
+    return (text+suffix).ljust(7)
+def main():
+    grammars = {
+        "parser": Parser(),
+        "acodegen": AbstractCodeGen(),
+        "xcodegen": X86CodeGen(),
+        "gas": GasGen(),
+        "assembler": Assembler(),
+        "xrunner": X86Runner(),
+    }
+    try:
+        expr = sys.stdin.read().strip()
+        for grammar_name in sys.argv[1:]:
+            if grammar_name.startswith("@"):
+                with open(grammar_name[1:], "w") as f:
+                    f.write(str(expr))
+                continue
+            grammar = grammars[grammar_name]
+            print_box(grammar.__class__.__name__)
+            expr = grammar.run("expr", expr)
+            print_expr(expr)
+    except _MatchError as e:
+        sys.stderr.write(e.describe())
+def print_expr(expr):
+    if isinstance(expr, str):
+        print(expr.strip())
+    else:
+        pprint.pprint(expr, width=20)
+def print_box(name):
+    print("")
+    print("=V{}==".format(" {} ".format(name).center(28, "=")))
+    print("")
+class X86Runner(object):
+
+    def run(self, name, machine_code):
+        import ctypes
+        import mmap
+        libc = ctypes.cdll.LoadLibrary(None)
+        fn_mmap = libc.mmap
+        fn_mmap.restype = ctypes.c_void_p
+        fn_mmap.argtypes = (
+            ctypes.c_void_p,
+            ctypes.c_size_t,
+            ctypes.c_int,
+            ctypes.c_int,
+            ctypes.c_int,
+            ctypes.c_size_t,
+        )
+        code = "".join([chr(x) for x in machine_code])
+        code_address = fn_mmap(
+            None,
+            len(code),
+            mmap.PROT_READ|mmap.PROT_WRITE|mmap.PROT_EXEC,
+            mmap.MAP_PRIVATE|mmap.MAP_ANONYMOUS,
+            -1,
+            0
+        )
+        ctypes.memmove(code_address, code, len(code))
+        fn_malloc = libc.malloc
+        fn_malloc.restype = ctypes.c_void_p
+        fn_malloc.argtypes = (ctypes.c_size_t,)
+        expr_fn_type = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_void_p)
+        expr_fn = ctypes.cast(code_address, expr_fn_type)
+        result = expr_fn(fn_malloc(1024))
+        return result
 class Op(object):
 
     def __init__(self, fn, prec, assoc):
@@ -20,9 +86,6 @@ def parseOps(expr, items, min_level=0):
         expr = op.fn(expr, parseOps(rhs, items, next_level))
     return expr
 
-def pad(text, suffix):
-    return (text+suffix).ljust(7)
-
 def modRmDirect(register):
     return 0xc0 | register
 
@@ -40,71 +103,3 @@ def littleEndian(number, numBytes):
         values.append(number & 0xff)
         number >>= 8
     return values
-
-class X86Runner(object):
-
-    def run(self, name, instructions):
-        import ctypes
-        import mmap
-        libc = ctypes.cdll.LoadLibrary(None)
-        fn_mmap = libc.mmap
-        fn_mmap.restype = ctypes.c_void_p
-        fn_mmap.argtypes = (
-            ctypes.c_void_p,
-            ctypes.c_size_t,
-            ctypes.c_int,
-            ctypes.c_int,
-            ctypes.c_int,
-            ctypes.c_size_t,
-        )
-        code = "".join([chr(x) for x in instructions])
-        code_address = fn_mmap(
-            None,
-            len(code),
-            mmap.PROT_READ|mmap.PROT_WRITE|mmap.PROT_EXEC,
-            mmap.MAP_PRIVATE|mmap.MAP_ANONYMOUS,
-            -1,
-            0
-        )
-        ctypes.memmove(code_address, code, len(code))
-        fn_malloc = libc.malloc
-        fn_malloc.restype = ctypes.c_void_p
-        fn_malloc.argtypes = (ctypes.c_size_t,)
-        stack_address = fn_malloc(1024)
-        expr_fn_type = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_void_p)
-        expr_fn = ctypes.cast(code_address, expr_fn_type)
-        return expr_fn(stack_address)
-def main():
-    grammars = {
-        "parser": Parser(),
-        "acodegen": AbstractCodeGen(),
-        "xcodegen": X86CodeGen(),
-        "gas": GasGen(),
-        "assembler": Assembler(),
-        "xrunner": X86Runner(),
-    }
-    try:
-        expr = sys.stdin.read().strip()
-        for grammar_name in sys.argv[1:]:
-            if grammar_name.startswith("@"):
-                with open(grammar_name[1:], "w") as f:
-                    f.write(str(expr))
-                continue
-            grammar = grammars[grammar_name]
-            print_expr(expr)
-            print_box(grammar.__class__.__name__)
-            expr = grammar.run("expr", expr)
-        print_expr(expr)
-    except _MatchError as e:
-        sys.stderr.write(e.describe())
-
-def print_expr(expr):
-    if isinstance(expr, str):
-        print(expr.strip())
-    else:
-        pprint.pprint(expr, width=20)
-
-def print_box(name):
-    print("")
-    print("=V{}==".format(" {} ".format(name).center(28, "=")))
-    print("")
