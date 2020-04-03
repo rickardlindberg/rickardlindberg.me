cad141cb15dd73f0c4cda3c47b22c3fc4aa6332c
Rickard Lindberg <ricli85@gmail.com>
2020-04-03 13:52:58 +0200

    Finish post on wx layout/update.

--- writing/wx-layout-update/generate_html.sh
+++ writing/wx-layout-update/generate_html.sh
@@ -3,11 +3,10 @@
 gen() {
     html=$(rliterate testlayout.rliterate --html)
     echo "---"
-    echo "title: 'DRAFT: $(echo "$html" | head -n1 | cut -c52- | cut -d'<' -f1)'"
-    echo "date: $(date +%Y-%m-%d)"
-    echo "tags: wxpython,rliterate,draft"
+    echo "title: '$(echo "$html" | head -n1 | cut -c52- | cut -d'<' -f1)'"
+    echo "date: 2020-04-03"
+    echo "tags: wxpython,rliterate"
     echo "---"
-    echo "<strong>This is a work in progress that will change. Like to see it finished? Let me know by sending me an email.</strong>"
     echo "$html" | tail -n+2
 }
 
