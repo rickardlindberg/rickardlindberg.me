ab897c483ebc24968d295978d943f05135bc733c
Rickard Lindberg <ricli85@gmail.com>
2020-04-09 07:28:11 +0200

    New draft.

---
+++ writing/rlmeta-cpp/generate_html.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+gen() {
+    html=$(rliterate rlmeta.rliterate --html)
+    echo "---"
+    echo "title: 'DRAFT: $(echo "$html" | head -n1 | cut -c52- | cut -d'<' -f1)'"
+    echo "date: $(date +%Y-%m-%d)"
+    echo "tags: rlmeta,draft"
+    echo "---"
+    echo "<strong>This is a work in progress that will change. Like to see it finished? Let me know by sending me an email.</strong>"
+    echo "$html" | tail -n+2
+}
+
+gen > index.html
