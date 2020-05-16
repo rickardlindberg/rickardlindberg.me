c89de890263289403bd45e211274aa27b6bc08c1
Rickard Lindberg <ricli85@gmail.com>
2020-05-16 14:32:26 +0200

    Clean up stat script.

--- writing/rlmeta-poster2/perfstat.sh
+++ writing/rlmeta-poster2/perfstat.sh
@@ -2,73 +2,85 @@
 
 set -e
 
-CLONE=/tmp/rlmeta-perf
-POST_PATH=
-GRAPHS_FILE=/tmp/rlmeta-perf.py
+CLONE_PATH=/tmp/rlmeta-perf
+GRAPH_FILE=/tmp/rlmeta-perf.py
 
-overall() {
+measure_overall() {
     (time ./compile.sh rlmeta.py > /dev/null) 2>&1 | ack --output='$1' 'real.*0m(.*)s$'
 }
 
-single_grammar() {
+measure_single_grammar() {
     python -m cProfile -s tottime rlmeta.py < parser.rlmeta | ack --output='$1' 'calls.*in (.*) seconds'
 }
 
-stat() {
-    python -c 'import sys; numbers = [float(x) for x in sys.stdin.read().splitlines()]; avg = sum(float(x) for x in numbers)/len(numbers); med = sorted(numbers)[len(numbers)/2-1]; sys.stderr.write("Numbers = {}\n".format(numbers)); sys.stderr.write(" Average = {}\n".format(avg)); sys.stderr.write("  Median = {}\n".format(med)); sys.stdout.write("{},\n{},\n".format(avg, med))'
+avg_med_numbers() {
+    python <(cat <<EOF
+import sys
+
+numbers = [float(x) for x in sys.stdin.read().splitlines()]
+
+avg = sum(numbers)/len(numbers);
+med = sorted(numbers)[(len(numbers)-1)/2] # Assumes odd number of numbers
+
+sys.stderr.write("Numbers = {}\n".format(numbers))
+sys.stderr.write(" Average = {}\n".format(avg))
+sys.stderr.write("  Median = {}\n".format(med))
+sys.stdout.write("{},\n{},\n".format(avg, med))
+EOF
+)
 }
 
-doit() {
-    echo "add(" | tee --append "$GRAPHS_FILE"
-    echo "'$1'," | tee --append "$GRAPHS_FILE"
+measure_all() {
+    echo "add(" | tee --append "$GRAPH_FILE"
+    echo "'$1'," | tee --append "$GRAPH_FILE"
     echo "Overall:"
     (
-        overall
-        overall
-        overall
-        overall
-        overall
-        overall
-        overall
-        overall
-        overall
-        overall
-        overall
-        overall
-        overall
-        overall
-        overall
-    ) | stat | tee --append "$GRAPHS_FILE"
+        measure_overall
+        measure_overall
+        measure_overall
+        measure_overall
+        measure_overall
+        measure_overall
+        measure_overall
+        measure_overall
+        measure_overall
+        measure_overall
+        measure_overall
+        measure_overall
+        measure_overall
+        measure_overall
+        measure_overall
+    ) | avg_med_numbers | tee --append "$GRAPH_FILE"
     echo "Single grammar:"
     (
-        single_grammar
-        single_grammar
-        single_grammar
-        single_grammar
-        single_grammar
-        single_grammar
-        single_grammar
-        single_grammar
-        single_grammar
-        single_grammar
-        single_grammar
-        single_grammar
-        single_grammar
-        single_grammar
-        single_grammar
-    ) | stat | tee --append "$GRAPHS_FILE"
-    echo ")" | tee --append "$GRAPHS_FILE"
+        measure_single_grammar
+        measure_single_grammar
+        measure_single_grammar
+        measure_single_grammar
+        measure_single_grammar
+        measure_single_grammar
+        measure_single_grammar
+        measure_single_grammar
+        measure_single_grammar
+        measure_single_grammar
+        measure_single_grammar
+        measure_single_grammar
+        measure_single_grammar
+        measure_single_grammar
+        measure_single_grammar
+    ) | avg_med_numbers | tee --append "$GRAPH_FILE"
+    echo ")" | tee --append "$GRAPH_FILE"
 }
 
-do_on_commit() {
-    (cd "$CLONE" && git checkout "$1" && cd "$2" && doit "$3")
+measure_on_commit() {
+    (cd "$CLONE_PATH" && git checkout "$1" && cd "$2" && measure_all "$3")
 }
 
-rm -rf "$CLONE"
+rm -rf "$CLONE_PATH"
 
-git clone ~/rickardlindberg.me/ "$CLONE"
+git clone ~/rickardlindberg.me/ "$CLONE_PATH"
 
-cat <<EOF > "$GRAPHS_FILE"
+cat <<EOF > "$GRAPH_FILE"
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -87,11 +99,11 @@ def add(name, overall_avg, overall_med, single_avg, single_med):
     single_grammars_avg.append(single_avg)
     single_grammars_med.append(single_med)
 EOF
-do_on_commit "c5d035acedfa3485d178a453ef2c887954478e44" "writing/rlmeta-vm" "vm original"
-do_on_commit "6b933e64e4d92809fd4464b8f9f6d2d6db6b65ad" "writing/rlmeta-memoize-failures/vm" "vm memoizing"
-do_on_commit "1db522231ae28add05ee22d1273b6d9a27933505" "writing/rlmeta-poster2" "poster v1"
-doit "tip"
-cat <<EOF >> "$GRAPHS_FILE"
+measure_on_commit "c5d035acedfa3485d178a453ef2c887954478e44" "writing/rlmeta-vm" "vm original"
+measure_on_commit "6b933e64e4d92809fd4464b8f9f6d2d6db6b65ad" "writing/rlmeta-memoize-failures/vm" "vm memoizing"
+measure_on_commit "1db522231ae28add05ee22d1273b6d9a27933505" "writing/rlmeta-poster2" "poster v1"
+measure_all "tip"
+cat <<EOF >> "$GRAPH_FILE"
 ind = np.arange(len(overalls_avg))  # the x locations for the groups
 width = 0.15  # the width of the bars
 padding = 0.02
@@ -120,4 +132,4 @@ fig.tight_layout()
 plt.show()
 EOF
 
-python "$GRAPHS_FILE"
+python "$GRAPH_FILE"
