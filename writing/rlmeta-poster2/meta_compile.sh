4934b9105ec609de2d59fce955d41879ef57fcea
Rickard Lindberg <ricli85@gmail.com>
2020-05-26 10:52:28 +0200

    Disallow semantic action in the middle.

--- writing/rlmeta-poster2/meta_compile.sh
+++ writing/rlmeta-poster2/meta_compile.sh
@@ -10,6 +10,11 @@ set -e
 
 ./compile.sh rlmeta3.py > rlmeta4.py
 
+fail() {
+    echo "FAIL!"
+    return 1
+}
+
 echo "Test: Reproduces itself"
 diff rlmeta3.py rlmeta4.py
 
@@ -17,7 +22,10 @@ echo "Test: Has its own support library embedded"
 diff support.py <(python rlmeta4.py --support)
 
 echo "Test: Error reporting string input"
-echo "Grammar { foo = . " | python rlmeta4.py && false
+echo "Grammar { foo = . " | python rlmeta4.py && fail
+
+echo "Test: Disallow semantic action in the middle"
+echo "Grammar { x = . ->[] . }" | (python rlmeta4.py 2>&1) > /dev/null && fail
 
 mv rlmeta4.py rlmeta3.py
 
