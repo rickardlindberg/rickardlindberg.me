0a6c1d70a8dd8b476c1392d271df06db8d656107
Rickard Lindberg <rickard@rickardlindberg.me>
2024-04-05 06:45:24 +0200

    Fix Args/args.

--- writing/output-tracking-vs-mocks/example.py
+++ writing/output-tracking-vs-mocks/example.py
@@ -37,40 +37,44 @@ class App:
             save_command=SaveCommand.create(),
             share_command=ShareCommand.create(),
             terminal=Terminal.create(),
+            args=Args.create(),
         )
 
     @classmethod
-    def create_null(cls, events):
+    def create_null(cls, events, args):
         return cls(
             save_command=SaveCommand.create_null().track_events(events),
             share_command=ShareCommand.create_null().track_events(events),
             terminal=Terminal.create_null().track_events(events),
+            args=Args.create_null(args=args),
         )
 
-    def __init__(self, save_command, share_command, terminal):
+    def __init__(self, save_command, share_command, terminal, args):
         self.save_command = save_command
         self.share_command  = share_command
         self.terminal = terminal
+        self.args = args
 
-    def run(self, args):
+    def run(self):
         """
         I dispatch to the correct sub-command:
 
         >>> events = Events()
-        >>> App.create_null(events).run(["save", "message"])
+        >>> App.create_null(events, args=["save", "message"]).run()
         >>> events
         SAVE_COMMAND ['message']
 
         >>> events = Events()
-        >>> App.create_null(events).run(["share"])
+        >>> App.create_null(events, args=["share"]).run()
         >>> events
         SHARE_COMMAND []
 
         >>> events = Events()
-        >>> App.create_null(events).run(["unknown", "sub", "command"])
+        >>> App.create_null(events, args=["unknown", "sub", "command"]).run()
         >>> events
         TERMINAL_WRITE 'Unknown command.'
         """
+        args = self.args.get()
         if args[0:1] == ["save"]:
             self.save_command.run(args[1:])
         elif args[0:1] == ["share"]:
@@ -134,9 +138,7 @@ class Terminal(Trackable):
     def write(self, text):
         """
         >>> events = Events()
-        >>> terminal = Terminal.create()
-        >>> terminal.track_events(events)
-        >>> terminal.write("hello")
+        >>> Terminal.create().track_events(events).write("hello")
         hello
         >>> events
         TERMINAL_WRITE 'hello'
@@ -145,6 +147,32 @@ class Terminal(Trackable):
         """
         self.notify(f"TERMINAL_WRITE {text!r}")
         print(text, file=self.sys.stdout, flush=True)
+
+class Args(Trackable):
+
+    @classmethod
+    def create(cls):
+        return cls(sys=sys)
+
+    @classmethod
+    def create_null(cls, args):
+        class NullSysModule:
+            argv = ["null program"]+args
+        return cls(sys=NullSysModule())
+
+    def __init__(self, sys):
+        Trackable.__init__(self)
+        self.sys = sys
+
+    def get(self):
+        """
+        >>> Args.create().get()
+        []
+
+        >>> Args.create_null(args=["test"]).get()
+        ['test']
+        """
+        return self.sys.argv[1:]
 
 if __name__ == "__main__":
     doctest.testmod()
