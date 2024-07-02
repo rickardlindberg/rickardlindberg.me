4d70aee5edfe9d277cd7632c2d53718557fac9f5
Rickard Lindberg <rickard@rickardlindberg.me>
2024-07-02 19:59:07 +0200

    Newsletterify script.

---
+++ bin/newsletterify.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import re
+import sys
+
+class Paragraph:
+
+    def __init__(self):
+        self.lines = []
+
+    def add_markdown_line(self, line):
+        self.lines.append(line)
+
+    def flush(self):
+        if self.lines:
+            print("")
+            print(" ".join(self.lines))
+            self.lines.clear()
+
+dividers = 0
+paragraph = Paragraph()
+for line in sys.stdin.read().splitlines():
+    if line == "---":
+        dividers += 1
+    elif dividers == 1 and line.startswith("title:"):
+        header = " ".join(line.split('"')[-2].split(" ")[1:])
+        print(f"# {header}")
+    elif dividers == 2 and line == "":
+        paragraph.flush()
+    elif dividers == 2:
+        paragraph.add_markdown_line(re.sub(
+            r"[(]/(.*?)/index.html[)]",
+            r"(http://rickardlindberg.me/\1/)",
+            line
+        ))
+paragraph.flush()
