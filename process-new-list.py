c4d2ba427f12cf91fe89e3f791de9c74f9a5f87e
Rickard Lindberg <rickard@rickardlindberg.me>
2024-08-10 18:16:03 +0200

    Fix date format.

--- process-new-list.py
+++ process-new-list.py
@@ -4,7 +4,7 @@ with open("new-list.txt") as f:
     for line in f:
         if line.strip():
             date, title = line.strip().split(": ", 1)
-            x = time.strptime(date, "%Y-%m-%d")
+            x = time.strptime(date, "%b %d, %Y")
             y = time.strftime("%e %B %Y", x)
             foo = " This post has not yet been imported to my new blog. In the meantime, you can read it here: …"
             print(f"{title.replace(foo, '')} - {y.strip()}")
